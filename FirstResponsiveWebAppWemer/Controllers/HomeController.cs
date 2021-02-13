15bb8d4ee557d1069c525c4b16054122a8175201
colbywemer <78008250+colbywemer@users.noreply.github.com>
2021-02-12 20:03:23 -0600

    ResponsiveWebApp

---
+++ FirstResponsiveWebAppWemer/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppWemer.Models;
+namespace FirstResponsiveWebAppWemer.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = null;
+            ViewBag.Age = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AgeCalculartor model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = model.AgeThisYear();
+                ViewBag.Name = model.Name;
+            }
+            else
+            {
+                ViewBag.Age = 0;
+                ViewBag.Name = null;
+            }
+            return View(model);
+        }
+        
+    }
+}
---
+++ FirstResponsiveWebAppWemer/Models/AgeCalculartor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppWemer.Models
+{
+    public class AgeCalculartor
+    {
+        [Required(ErrorMessage ="Please enter your name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your birthday.")]
+        public DateTime? Birthday { get; set; }
+        public int? AgeThisYear() 
+        {
+            DateTime Birth = Convert.ToDateTime(Birthday);
+            int age = (new DateTime(DateTime.Now.Subtract(Birth).Ticks).Year)- 1;
+            return age;
+        }
+       
+    }
+}
